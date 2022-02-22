b6ffafeabc7747e74facd07ddc5688cb586de292
gleaming0 <aheun1216@naver.com>
2022-02-22 13:55:04 +0900

    variable declare

---
+++ javascriptBasic/prac01.js
@@ -0,0 +1,16 @@
+var age = 33;
+var name = "hong";
+var name = "gil";
+//var의 경우, 변수명이 같아도 오류가 안난다.
+
+console.log(name);
+
+let c = "test";
+let d = "test2";
+//let d = "test2";
+//let은 변수명이 같으면 오류가 난다!
+
+const e = 11;
+console.log(e);
+//e = 12;
+//const는 상수이므로, 값 변경이 안된다. 선언시 초기화를 해야 한다.
