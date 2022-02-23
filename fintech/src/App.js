44d7df2370aa9e54588171473b1aa21bf06520f0
gleaming0 <aheun1216@naver.com>
2022-02-23 20:17:15 +0900

    create header component

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,22 +1,12 @@
 import Welcome from './components/Welcome';
 import { useState } from "react"; //state hook을 import
 import ListComponent from './components/ListComponent';
+import HeaderWhite from './components/HeaderWhite';
 
 function App() {
-  let username = "미입력";
-  let [usernameWithState, setUsernameWithState] = useState("초기값");
-  console.log(usernameWithState); //Console 출력 확인(리렌더링o)
-
-  const handleInputChange = (e) => {
-    setUsernameWithState(e.target.value);
-    //console.log(username); //Console 출력 확인(리렌더링x)
-  };
-
   return (
     <div>
-      이름 : <input onChange={handleInputChange}></input>
-      <h1>{usernameWithState}</h1>
-      {/* {주석처리는 ctrl + /} */}
+      <HeaderWhite title="메인" />
       <ListComponent></ListComponent>
     </div>
   );
