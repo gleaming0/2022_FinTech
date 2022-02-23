126b75d5bbb9a4e5932d179b8a40b969bb718d86
gleaming0 <aheun1216@naver.com>
2022-02-23 13:02:38 +0900

    store components separately

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,8 +1,4 @@
-/* Welcome이라는 컴포넌트 만들기! 프롭스(username, age) 사용!*/
-const Welcome = (props) => {
-  console.log(props);
-  return (<div>안녕하세요. {props.age}세 {props.username}님 반갑습니다!</div>);
-}
+import Welcome from './components/Welcome';
 
 function App() {
   return (
--- fintech/src/components/Welcome.js
+++ fintech/src/components/Welcome.js
@@ -1,10 +1,8 @@
 const Welcome = (props) => {
-	console.log(props);
-	return (
-        <div>
-            안녕하세요 {props.age}세 {props.username} 님 반갑습니다!
-        </div>
+    console.log(props);
+    return (
+        <div>안녕하세요. {props.age}세 {props.username}님 반갑습니다!</div>
     );
-};
+}
 
 export default Welcome;
