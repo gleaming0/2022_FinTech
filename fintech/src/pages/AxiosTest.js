53acda53b90a051eb76e5499e46ec8699e60291f
gleaming0 <aheun1216@naver.com>
2022-02-23 10:54:56 +0900

    use external module -export default

---
+++ fintech/src/pages/AxiosTest.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeaderWhite from '../components/HeaderWhite'
+
+const AxiosTest = () => {
+    return (
+        <div>
+            <HeaderWhite title="http 통신" />
+            <button>데이터 요청하기</button>
+        </div>
+    );
+};
+
+export default AxiosTest;
