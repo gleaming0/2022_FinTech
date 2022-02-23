87c1d9c41ee25b0d9a9b79dae926cfe3286eb369
gleaming0 <aheun1216@naver.com>
2022-02-23 21:15:30 +0900

    add AxiosTest page route, page

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import ListComponent from './components/ListComponent';
+import AxiosTest from "./pages/AxiosTest";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/test" element={<ListComponent/>}></Route>
+        <Route path="/test" element={<ListComponent />}></Route>
       </Routes>
-
       <Routes>
-        <Route path="/" element={<Welcome/>}></Route>
+        <Route path="/" element={<Welcome />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/axiosTest" element={<AxiosTest />}></Route>
       </Routes>
     </BrowserRouter>
 
---
+++ fintech/src/pages/AxiosTest.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderWhite from '../components/HeaderWhite';
+
+
+const AxiosTest = () => {
+  return (
+    <div>
+        <HeaderWhite title="HTTP 통신" />
+        <button>데이터 요청하기</button>
+    </div>
+  );
+};
+
+export default AxiosTest
