4ef4e1fe017ee6bc66fc916c0584dc559cfeeade
gleaming0 <aheun1216@naver.com>
2022-02-23 10:20:14 +0900

    npm install react-router-dom

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -6,18 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          이걸 바꾸면?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> 이걸 바꾸면!? </p>
+        
       </header>
     </div>
   );
