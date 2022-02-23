3ff18cd51492b91cde4f0b6b0b9f2e5bf4af081d
gleaming0 <aheun1216@naver.com>
2022-02-23 23:19:12 +0900

    work5 news search application

--- fintech/src/pages/NewsApiPage.js
+++ fintech/src/pages/NewsApiPage.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderWhite from "../components/HeaderWhite";
+import SearchInput from "../components/News/SearchInput";
 
+/*
+    searchText : 검색어를 입력할 곳
+    searchResultList : 검색 결과를 저장할 곳, request에서 배열([])형태로 받아옴
+    handleSearchTextChange : searchText가 변경되었을 때 동작하는 이벤트
+    handleSearchButtonClick : searchText 입력 후 버튼을 눌렀을 때 동작하는 이벤트
+*/
 const NewsApiPage = () => {
+  const [searchText, setUsers] = useState("");
+  const [searchResultList, setSearchResultList] = useState([]);
+
+  const handleSearchTextChange = () => {
+    //input 변경사항을 search Text 반영
+  };
+
+  const handleSearchButtonClick = () => {
+    //axios 통해 newsList 요청하기
+  };
+
   return (
     <div>
         <HeaderWhite title="뉴스 검색"></HeaderWhite>
+        <SearchInput
+          handleChange={handleSearchTextChange}
+          handleClick={handleSearchButtonClick}
+        ></SearchInput>
+        <NewsList searchResultList={searchResultList}></NewsList>
     </div>
   );
 };
