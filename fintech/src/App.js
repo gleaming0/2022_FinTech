58d7d4a9965a9c405719400c2d240e8d4381db5e
gleaming0 <aheun1216@naver.com>
2022-02-23 18:22:30 +0900

    practice react state, useState

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,10 +1,21 @@
 import Welcome from './components/Welcome';
+import { useState } from "react"; //state hook을 import
 
 function App() {
+  let username = "미입력";
+  let [usernameWithState, setUsernameWithState] = useState("초기값");
+  //console.log(usernameWithState); //Console 출력 확인(리렌더링o)
+
+  const handleInputChange = (e) => {
+    setUsernameWithState(e.target.value);
+    //console.log(username); //Console 출력 확인(리렌더링x)
+  };
+
   return (
     <div>
-      <Welcome username="홍길동" age="22"></Welcome>
-      <Welcome username="김아은" age="22"></Welcome>
+      이름 : <input onChange={handleInputChange}></input>
+      <h1>{usernameWithState}</h1>
+      {/* {주석처리는 ctrl + /} */}
     </div>
   );
 }
