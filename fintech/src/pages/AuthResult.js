bf83efa17945d1b8bc66a04157df5e56a9fc77c6
gleaming0 <aheun1216@naver.com>
2022-03-02 22:27:45 +0900

    쿼리스트링 -> js 오브젝트로 변경

--- fintech/src/pages/AuthResult.js
+++ fintech/src/pages/AuthResult.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import HeaderWhite from '../components/HeaderWhite';
+import { useLocation } from "react-router-dom";
+import queryString from 'query-string';
 
 const AuthResult = () => {
+    console.log(useLocation().search); //-> 쿼리형태로 나열되어 보임
+    const parsedResult = queryString.parse(useLocation().search);
+    console.log(parsedResult); //-> 오브젝트로 예쁘게 보임
+    //console.log(parsedResult.code); //-> 오브젝트 중 code만 보임
   return (
-    <div>AuthResult</div>
-  )
-}
+    <div>
+        <HeaderWhite title={"AccessToken 요청"}></HeaderWhite>
+    </div>
+  );
+};
 
 export default AuthResult;
