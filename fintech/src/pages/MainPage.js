108b7918ff7534d1364b0a44079cd6432f2c9cf5
gleaming0 <aheun1216@naver.com>
2022-03-04 01:15:39 +0900

    계좌 목록에 카드 디자인 반영

--- fintech/src/pages/MainPage.js
+++ fintech/src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import HeaderWhite from '../components/HeaderWhite';
+import MainCard from '../components/Main/MainCard';
 
 const MainPage = () => {
     useEffect(() => { //자동으로 getAccountList 함수 실행하도록
@@ -35,15 +36,22 @@ const MainPage = () => {
         };
 
         axios(option).then(( data ) => {
-            console.log(data);
-        });
-
-        
+            const accountListFromRequest = data.res_list;
+            setaccountList(accountListFromRequest);
+        });        
     };
 
   return (
     <div>
         <HeaderWhite title={"메인페이지(계좌목록)"}></HeaderWhite>
+        {accountList.map(({ fintech_use_num, bank_name}) => {
+            return (
+                <MainCard
+                    bankName={ bank_name }
+                    fintechUseNo={ fintech_use_num }
+                ></MainCard>
+            );
+        })}
     </div>
   );
 };
